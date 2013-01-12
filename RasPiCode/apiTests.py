35b3b3e734c0c7d33fa954858d2b267ac16679c0
Brad <bradluyster+git@gmail.com>
2013-01-12 18:52:43 -0500

    requesting data from lvl1 member database now works.

--- RasPiCode/apiTests.py
+++ RasPiCode/apiTests.py
@@ -1,15 +1,20 @@
 from generateApiJson import generate
 import urllib2
 import json
-from API_KEY import STORE_API_PRIVATE_KEY
+from API_KEY import STORE_API_PRIVATE_KEY, LVL1_SQL_USER, LVL1_SQL_PASSWORD
+import MySQLdb as mdb
 
 STORE_API_URL = "http://192.168.1.104/opencart/index.php?route=api/api"
+LVL1_SQL_URL = "mysql.lvl1.org"
 
 def get_product_list(order_id):
     api_request = {'command': 'getProductsByInvoice', 'data': str(order_id)}
     full_request = STORE_API_URL + "&" + generate(STORE_API_PRIVATE_KEY, api_request)
     http_request = urllib2.Request(url=full_request)
-    response = urllib2.urlopen(http_request)
+    try:
+        response = urllib2.urlopen(http_request)
+    except urllib2.URLError:
+        return (-1,)
 
     json_response = response.read()
 
@@ -24,24 +29,49 @@ def mark_order_complete(order_id):
     api_request = {'command': 'orderFulfilled', 'data': str(order_id)}
     full_request = STORE_API_URL + "&" + generate(STORE_API_PRIVATE_KEY, api_request)
     http_request = urllib2.Request(url=full_request)
-    response = urllib2.urlopen(http_request)
+    try:
+        response = urllib2.urlopen(http_request)
+    except urllib2.URLError:
+        return False
 
     json_response = response.read()
     return json.loads(json_response)['data']
 
 def create_order(card_number, item_number):
-    firstname = "Test"
-    lastname = "Python"
-    email = "TestPython@example.com"
+
+    try:
+        con = mdb.connect(LVL1_SQL_URL, LVL1_SQL_USER, LVL1_SQL_PASSWORD, 'lvl1_members')
+        cur = con.cursor(mdb.cursors.DictCursor)
+        count = cur.execute("SELECT `member`.`id`, `first_name`, `last_name`, `email_address` FROM `member`"
+            "LEFT JOIN `email` ON `member`.`id` = `email`.`member_id` WHERE `member`.`is_active` = 1 AND `member`.`key_id` = %s LIMIT 1", card_number)
+        if(count > 0):
+            data = cur.fetchoneDict()
+            firstname = data['first_name']
+            lastname = data['last_name']
+            email = data['email_address']
+        else:
+            return False
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+    finally:
+        if con:
+            con.close()
+
+
     slot = item_number
     api_request = {'command': 'createOrder', 'data': {'firstname': firstname, 'lastname': lastname, 'email': email, 'slot_number': slot}}
 
     full_request = STORE_API_URL + "&" + generate(STORE_API_PRIVATE_KEY, api_request)
     http_request = urllib2.Request(url=full_request)
-    response = urllib2.urlopen(http_request)
+    try:
+        response = urllib2.urlopen(http_request)
+    except urllib2.URLError:
+        return False
+
+    #response = urllib2.urlopen(http_request)
 
     json_response = response.read()
     return json.loads(json_response)['data']
 
 if __name__ == '__main__':
-    print create_order(1238488483, 1)
+    print create_order("05A0A92", 1)
